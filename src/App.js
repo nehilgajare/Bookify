7611d062ed1d64ae85f22073a0a40890a66cdf8e
Nehil <nehil.gajare@rediffmail.com>
2023-11-01 01:18:22 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { Routes,Route } from "react-router-dom";
+
+//Pages
+import RegisterPage from "./pages/Register";
+
+//CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/login" element={<h1>Login</h1>} />
+      <Route path="/register" element={<RegisterPage />} />
+
+      
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from "react-router-dom"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FirebaseProvider } from './context/FIrebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <FirebaseProvider>
+        <App />
+      </FirebaseProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
